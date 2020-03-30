12d162ba513c3e8a82b72cde0884674576dd0de1
StephonAutery <stephon@stephonautery.com>
2020-03-29 20:03:16 -0700

    most functionality working. datastore pending.

--- js/jscript.js
+++ js/jscript.js
@@ -1,81 +1,111 @@
-varCitySearch = "san leandro";
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + varCitySearch + '&appid=630f6b0a5dd5632e93ad38bae7f3f14b';
-let lat = "";
-let lon = "";
-var cityName;
-var queryURLuV = "";
-var queryURLHourly = "";
 
-async function init() {
-  var apiResponse;
-  function API() {
-    return $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+$(function () {
+
+  varCitySearch = "san leandro";
+  var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + varCitySearch + '&appid=630f6b0a5dd5632e93ad38bae7f3f14b';
+  let lat = "";
+  let lon = "";
+  var cityName;
+  var queryURLuV = "";
+  var queryURLHourly = "";
+
+  async function init(queryURL) {
+    var apiResponse;
+    function API() {
+      return $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+    }
+    apiResponse = await API();
+    showCityWeather(apiResponse);
+    initUv();
+    initForcast();
   }
-  apiResponse = await API();
-  showData(apiResponse);
-}
 
-init()
-initHourly()
-
-async function initUv() {
-  queryURLuV = 'https://api.openweathermap.org/data/2.5/uvi?appid=630f6b0a5dd5632e93ad38bae7f3f14b&lat=' + lat + '&lon=' + lon + '';
-  var apiResponseUv;
-  function API() {
-    return $.ajax({
-      url: queryURLuV,
-      method: "GET"
-    })
+  async function initUv() {
+    queryURLuV = 'https://api.openweathermap.org/data/2.5/uvi?&units-imperial&appid=630f6b0a5dd5632e93ad38bae7f3f14b&lat=' + lat + '&lon=' + lon + '';
+    var apiResponseUv;
+    function API() {
+      return $.ajax({
+        url: queryURLuV,
+        method: "GET"
+      })
+    }
+    apiResponseUv = await API();
+    showUVData(apiResponseUv);
   }
-  apiResponseUv = await API();
-  showDataUv(apiResponseUv);
-}
 
-async function initHourly() {
-  queryURLHourly = 'https://api.openweathermap.org/data/2.5/weather?q=' + varCitySearch + '&appid=630f6b0a5dd5632e93ad38bae7f3f14b';
-  var apiResponseHour;
-  function API() {
-    return $.ajax({
-      url: queryURLHourly,
-      method: "GET"
-    })
+  async function initForcast() {
+    queryURLforcast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + varCitySearch + '&units=imperial&appid=630f6b0a5dd5632e93ad38bae7f3f14b';
+    var apiResponseForcast;
+    function API() {
+      return $.ajax({
+        url: queryURLforcast,
+        method: "GET"
+      })
+    }
+    apiResponseForcast = await API();
+    showForcastData(apiResponseForcast);
   }
-  apiResponseHour = await API();
-  showDataHour(apiResponseHour);
-}
 
-function showDataHour(apiResponseHour) {
-  console.log("--------- hourly data");
-  console.log(apiResponseHour);
-  $("#city-name").text(apiResponseHour.name);
-  $("#humidity").text("humidity: " + apiResponseHour.main.humidity);
-  $("#wind-speed").text("wind speed: " + apiResponseHour.wind.speed);
-}
-
-function showData(apiResponse) {
-  console.log("--------- forcast data");
-  console.log(apiResponse);
-  var dailyW = apiResponse.list;
-
-  $("#date").text(dailyW[0].dt_txt);
-  for (let i = 1; i <= 25; i++) {
-    $("#tiles").append(
-      '<div class="col-2-xs text-left p-2"><p>' + dailyW[i].dt_txt + '</p><p>weather: ' + dailyW[i].weather[0].main + '</p><p>temp: ' + dailyW[i].main.temp + '</p><p>humidity: ' + dailyW[i].main.humidity + '</p></div>'
-    );
+  function showCityWeather(apiResponseHour) {
+    $("#city-name").text(apiResponseHour.name);
+    $("#humidity").text("humidity: " + apiResponseHour.main.humidity);
+    $("#wind-speed").text("wind speed: " + apiResponseHour.wind.speed);
+    lat = apiResponseHour.coord.lat;
+    lon = apiResponseHour.coord.lon;
+    initUv();
   }
-  lat = apiResponse.city.coord.lat;
-  lon = apiResponse.city.coord.lon;
-  initUv();
-}
 
-console.log(lat + " ..");
-console.log(lon + " ..");
+  function showUVData(apiResponseUv) {
+    $("#uv").text(apiResponseUv.value);
+    $("#uv").attr("class", "bg-danger");
+  }
 
-function showDataUv(apiResponseUv) {
-  console.log("---------------------- uv");
-  console.log(apiResponseUv);
-  $("#uv").text("uv index: " + apiResponseUv.value);
-}
+  function showForcastData(apiResponseForcast) {
+    console.log("--------- forcast data");
+    console.log(apiResponseForcast.city.name);
+    console.log(apiResponseForcast);
+    // stuff the dailyW variable full of the daily forcast data
+    var dailyW = apiResponseForcast.list;
+    var dateStart;
+    $("#tiles").empty();
+    $("#date").text(dailyW[0].dt_txt);
+    for (let i = 1; i <= 25; i++) {
+      dateStart = dailyW[i].dt_txt;
+      // console.log(dateStart + " ....");
+      dateStart = toString(dateStart);
+      if (dateStart.search("00:00:00")) {
+        // console.log(dateStart);
+        $("#tiles").append(
+          '<div class="col-2-xs m-2 text-left p-2 rounded bg-primary"><p>' + dailyW[i].dt_txt + '</p><p><img src="./images/' + dailyW[i].weather[0].icon + '@2x.png"></p><p>temp: ' + dailyW[i].main.temp +  '</p><p>humidity: ' + dailyW[i].main.humidity + '</p></div>'
+        );
+      }
+    }
+  }
+
+  init(queryURL);
+
+  $("#submit").on("click", function (event) {
+    varCitySearch = $("#city-input").val();
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + varCitySearch + '&appid=630f6b0a5dd5632e93ad38bae7f3f14b';
+
+    async function initI(queryURL) {
+      var apiResponse;
+      function API() {
+        return $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+      }
+      apiResponse = await API();
+      init(queryURL);
+    }
+    initI(queryURL);
+  })
+
+  console.log(lat + " ..");
+  console.log(lon + " ..");
+
+}); 
